81a0e73f2648e6e8b07cfcda32cd7d274096df4c
Crissdan93 <136115058+Crissdan93@users.noreply.github.com>
2023-06-20 10:11:56 -0600

    Add files via upload

---
+++ tarea1.java
@@ -0,0 +1,27 @@
+package tareasJava;
+
+import java.util.Scanner;
+
+public class tarea1 {
+	public static void main(String[] arg) {
+	
+		Scanner input = new Scanner(System.in);
+		
+		//AREA DE RECTANGULO
+		
+		System.out.println("Ingrese el largo del rectangulo ");
+		int valorLargo = input.nextInt();
+		
+		System.out.println("Ingresar el ancho del rectangulo ");
+		int valorAncho = input.nextInt();
+		
+		int area= valorLargo * valorAncho;
+		
+		
+		System.out.println("El area del rectangulo es " + area);
+		
+		
+		//AREA DE RECTANGULO TAREA 1
+	}
+
+}
