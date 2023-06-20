174cc3766927ff4dfff8520e20ad570832546e77
Crissdan93 <136115058+Crissdan93@users.noreply.github.com>
2023-06-20 10:09:16 -0600

    Add files via upload

---
+++ tareas2.java
@@ -0,0 +1,30 @@
+package tareasJava2;
+
+import java.util.Scanner;
+
+public class tareas2 {
+	
+	public static final double pi = 3.1416;
+	
+	public static void main(String[] arg){
+		
+		Scanner input = new Scanner(System.in);
+		
+		//AREA DE UN CIRCULO
+		
+		System.out.println("Ingrese el valor del radio ");
+		double radio = input.nextFloat();
+		
+		
+		double area= (radio*radio) * pi;
+		
+		
+		System.out.println("El area del circulo es " + area);
+		
+		
+		
+		//AREA DE UN CIRCULO TAREA s
+		
+	}
+
+}
